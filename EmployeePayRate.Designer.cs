2e4886e82cd5a0fa5d9bbed2aa9195e42387b27f
kehlert72 <kaleb.ehlert@gmail.com>
2020-07-27 17:17:35 -0400

    Completed project

---
+++ EmployeePayRate.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace EhlertProgram10
+{
+    partial class EmployeePayRate
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtEmployeeName = new System.Windows.Forms.TextBox();
+            this.txtEmployeeNumber = new System.Windows.Forms.TextBox();
+            this.txtEmployeePayRate = new System.Windows.Forms.TextBox();
+            this.txtEmployeeHoursWorked = new System.Windows.Forms.TextBox();
+            this.lblEmployeeName = new System.Windows.Forms.Label();
+            this.lblEmployeePayRate = new System.Windows.Forms.Label();
+            this.lblEmployeeNumber = new System.Windows.Forms.Label();
+            this.lblEmployeeHoursWorked = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.txtDisplay = new System.Windows.Forms.TextBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtEmployeeName
+            // 
+            this.txtEmployeeName.Location = new System.Drawing.Point(107, 34);
+            this.txtEmployeeName.Name = "txtEmployeeName";
+            this.txtEmployeeName.Size = new System.Drawing.Size(100, 20);
+            this.txtEmployeeName.TabIndex = 0;
+            // 
+            // txtEmployeeNumber
+            // 
+            this.txtEmployeeNumber.Location = new System.Drawing.Point(107, 60);
+            this.txtEmployeeNumber.Name = "txtEmployeeNumber";
+            this.txtEmployeeNumber.Size = new System.Drawing.Size(100, 20);
+            this.txtEmployeeNumber.TabIndex = 1;
+            // 
+            // txtEmployeePayRate
+            // 
+            this.txtEmployeePayRate.Location = new System.Drawing.Point(107, 86);
+            this.txtEmployeePayRate.Name = "txtEmployeePayRate";
+            this.txtEmployeePayRate.Size = new System.Drawing.Size(100, 20);
+            this.txtEmployeePayRate.TabIndex = 2;
+            // 
+            // txtEmployeeHoursWorked
+            // 
+            this.txtEmployeeHoursWorked.Location = new System.Drawing.Point(107, 112);
+            this.txtEmployeeHoursWorked.Name = "txtEmployeeHoursWorked";
+            this.txtEmployeeHoursWorked.Size = new System.Drawing.Size(100, 20);
+            this.txtEmployeeHoursWorked.TabIndex = 3;
+            // 
+            // lblEmployeeName
+            // 
+            this.lblEmployeeName.AutoSize = true;
+            this.lblEmployeeName.Location = new System.Drawing.Point(4, 34);
+            this.lblEmployeeName.Name = "lblEmployeeName";
+            this.lblEmployeeName.Size = new System.Drawing.Size(35, 13);
+            this.lblEmployeeName.TabIndex = 4;
+            this.lblEmployeeName.Text = "Name";
+            // 
+            // lblEmployeePayRate
+            // 
+            this.lblEmployeePayRate.AutoSize = true;
+            this.lblEmployeePayRate.Location = new System.Drawing.Point(4, 89);
+            this.lblEmployeePayRate.Name = "lblEmployeePayRate";
+            this.lblEmployeePayRate.Size = new System.Drawing.Size(99, 13);
+            this.lblEmployeePayRate.TabIndex = 5;
+            this.lblEmployeePayRate.Text = "Pay Rate (per hour)";
+            // 
+            // lblEmployeeNumber
+            // 
+            this.lblEmployeeNumber.AutoSize = true;
+            this.lblEmployeeNumber.Location = new System.Drawing.Point(4, 63);
+            this.lblEmployeeNumber.Name = "lblEmployeeNumber";
+            this.lblEmployeeNumber.Size = new System.Drawing.Size(44, 13);
+            this.lblEmployeeNumber.TabIndex = 6;
+            this.lblEmployeeNumber.Text = "Number";
+            // 
+            // lblEmployeeHoursWorked
+            // 
+            this.lblEmployeeHoursWorked.AutoSize = true;
+            this.lblEmployeeHoursWorked.Location = new System.Drawing.Point(4, 115);
+            this.lblEmployeeHoursWorked.Name = "lblEmployeeHoursWorked";
+            this.lblEmployeeHoursWorked.Size = new System.Drawing.Size(76, 13);
+            this.lblEmployeeHoursWorked.TabIndex = 7;
+            this.lblEmployeeHoursWorked.Text = "Hours Worked";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(284, 24);
+            this.menuStrip1.TabIndex = 8;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.saveToolStripMenuItem,
+            this.openToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // txtDisplay
+            // 
+            this.txtDisplay.Location = new System.Drawing.Point(7, 162);
+            this.txtDisplay.Multiline = true;
+            this.txtDisplay.Name = "txtDisplay";
+            this.txtDisplay.Size = new System.Drawing.Size(265, 87);
+            this.txtDisplay.TabIndex = 9;
+            // 
+            // EmployeePayRate
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.txtDisplay);
+            this.Controls.Add(this.lblEmployeeHoursWorked);
+            this.Controls.Add(this.lblEmployeeNumber);
+            this.Controls.Add(this.lblEmployeePayRate);
+            this.Controls.Add(this.lblEmployeeName);
+            this.Controls.Add(this.txtEmployeeHoursWorked);
+            this.Controls.Add(this.txtEmployeePayRate);
+            this.Controls.Add(this.txtEmployeeNumber);
+            this.Controls.Add(this.txtEmployeeName);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "EmployeePayRate";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtEmployeeName;
+        private System.Windows.Forms.TextBox txtEmployeeNumber;
+        private System.Windows.Forms.TextBox txtEmployeePayRate;
+        private System.Windows.Forms.TextBox txtEmployeeHoursWorked;
+        private System.Windows.Forms.Label lblEmployeeName;
+        private System.Windows.Forms.Label lblEmployeePayRate;
+        private System.Windows.Forms.Label lblEmployeeNumber;
+        private System.Windows.Forms.Label lblEmployeeHoursWorked;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.TextBox txtDisplay;
+    }
+}
+
---
+++ EmployeePayRate.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+/*  ID
+ *      Employee Pay Data, Kaleb Ehlert, 4/8/2018
+ *  Purpose
+ *      The purpose of the program is to implement GUI that stores and displays the name and total pay of the employee.
+ *  Form Controls
+ *      lblEmployeeName: A label for the text field that takes the name of the employee.
+ *      lblEmployeeNumber: A label for the text field that takes the number of the employee.
+ *      lblEmployeePayRate: A label for the text field that takes the pay rate of the employee.
+ *      lblEmployeeHoursWorked: A label for the text field that take the hours worked by the employee.
+ *      txtEmployeeName: A text field that either displays or takes the employee's name.
+ *      txtEmployeeNumber: A text field that either displays or takes the employee's number.
+ *      txtEmployeePayRate: A text field that either displays or takes the employee's pay rate.
+ *      txtEmployeeHoursWorked: A text field that either displays or takes the employee's hours worked.
+ *  Code & Logic
+ *      The input is either the keyboard text the user enters or a file's data.
+ *      The processing either writes to a file or organizes a files input.
+ *      The output is the stored data to a file or displaying the information to the user throught the GUI.
+ */
+
+namespace EhlertProgram10
+{
+    public partial class EmployeePayRate : Form
+    {
+        public EmployeePayRate()
+        {
+            InitializeComponent();
+        }
+
+        // Stores the name, number, and total of pay of the employee before deductions
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+
+            StreamWriter file;
+
+            double pay = 0.0;
+
+            // If pay is greater than fourty than calculate time and a half, otherwise just calculate the amount earned
+            if (double.Parse(txtEmployeeHoursWorked.Text) >= 40) {
+                pay = double.Parse(txtEmployeePayRate.Text) * int.Parse(txtEmployeeHoursWorked.Text) * 1.5;
+            } else {
+                pay = double.Parse(txtEmployeePayRate.Text) * int.Parse(txtEmployeeHoursWorked.Text);
+            }
+
+            // Get the file name and provide two filters
+            saveFileDialog.Filter = "(*.txt)|*.txt|All files (*.*)|*.*";
+            saveFileDialog.ShowDialog();
+
+            // Set the file to write to
+            file = new StreamWriter(saveFileDialog.FileName);
+
+            // Write the name and number of the employee to the file
+            file.WriteLine(txtEmployeeName.Text);
+            file.WriteLine(txtEmployeeNumber.Text);
+
+            // Write the amount the employee earned to the file
+            file.WriteLine(pay);
+
+            // Close the file
+            file.Close();
+        }
+
+        // Displays the stored information of the employee
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            StreamReader inFile;
+            string name = "";
+            string number = "";
+            string pay = "";
+
+            try {
+                // Find the file and set a filter
+                openFileDialog.Filter = "(*.txt)|*.txt|All files (*.*)|*.*";
+                openFileDialog.ShowDialog();
+
+                // Open the file
+                inFile = new StreamReader(openFileDialog.FileName);
+
+                // Read the file for the name, number, and pay
+                name = inFile.ReadLine();
+                number = inFile.ReadLine();
+                pay = inFile.ReadLine();
+            } catch {
+                MessageBox.Show("Error the file is unable to be open! The file might be corrupt.");
+            }
+
+            // Display the information on the GUI
+            if ( double.Parse(pay) - Math.Truncate(double.Parse(pay)) > 0 )
+                txtDisplay.Text = "Name: " + name + "\r\n" + "Total Pay: $" + pay + "\r\n";
+            else
+                txtDisplay.Text = "Name: " + name + "\r\n" + "Total Pay: $" + pay + ".00" + "\r\n";
+
+            // Close the file
+            openFileDialog.Dispose();
+        }
+    }
+}
